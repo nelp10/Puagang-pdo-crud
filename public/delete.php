aab117dfc8ded437d7ed280c890bc931a21413e4
nelp10 <20221805@nbsc.edu.ph>
2024-04-18 11:57:24 +0800

    big changes

--- public/delete.php
+++ public/delete.php
@@ -1,18 +1,19 @@
+
 <?php
 // Process delete operation after confirmation
-if(isset($_POST["id"]) && !empty($_POST["id"])){
+if(isset($_POST["productid"]) && !empty($_POST["productid"])){
     // Include config file
     require_once "config.php";
     
     // Prepare a delete statement
-    $sql = "DELETE FROM employees WHERE id = :id";
+    $sql = "DELETE FROM products WHERE product_id = :productid";
     
     if($stmt = $pdo->prepare($sql)){
         // Bind variables to the prepared statement as parameters
-        $stmt->bindParam(":id", $param_id);
+        $stmt->bindParam(":productid", $param_productid);
         
         // Set parameters
-        $param_id = trim($_POST["id"]);
+        $param_productid = trim($_POST["productid"]);
         
         // Attempt to execute the prepared statement
         if($stmt->execute()){
@@ -31,7 +32,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
     unset($pdo);
 } else{
     // Check existence of id parameter
-    if(empty(trim($_GET["id"]))){
+    if(empty(trim($_GET["productid"]))){
         // URL doesn't contain id parameter. Redirect to error page
         header("location: error.php");
         exit();
@@ -60,8 +61,8 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
                     <h2 class="mt-5 mb-3">Delete Record</h2>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="alert alert-danger">
-                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
-                            <p>Are you sure you want to delete this employee record?</p>
+                            <input type="hidden" name="productid" value="<?php echo trim($_GET["productid"]); ?>"/>
+                            <p>Are you sure you want to delete this product record?</p>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
                                 <a href="index.php" class="btn btn-secondary ml-2">No</a>
