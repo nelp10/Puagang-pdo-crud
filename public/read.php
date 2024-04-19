f56ec922bbf80526537cc74e239d9146f4d92fc5
nelp10 <20221805@nbsc.edu.ph>
2024-04-19 10:27:53 +0800

    add changes

--- public/read.php
+++ public/read.php
@@ -1,19 +1,18 @@
-
 <?php
 // Check existence of id parameter before processing further
-if(isset($_GET["productid"]) && !empty(trim($_GET["productid"]))){
+if(isset($_GET["product_id"]) && !empty(trim($_GET["product_id"]))){
     // Include config file
-    require_once "config.php";
+    require_once "../db/config.php";
     
     // Prepare a select statement
-    $sql = "SELECT * FROM products WHERE product_id = :productid";
+    $sql = "SELECT * FROM products WHERE product_id = :product_id";
     
     if($stmt = $pdo->prepare($sql)){
         // Bind variables to the prepared statement as parameters
-        $stmt->bindParam(":productid", $param_productid);
+        $stmt->bindParam(":product_id", $param_product_id);
         
         // Set parameters
-        $param_productid = trim($_GET["productid"]);
+        $param_product_id = trim($_GET["product_id"]);
         
         // Attempt to execute the prepared statement
         if($stmt->execute()){
@@ -23,13 +22,12 @@ if(isset($_GET["productid"]) && !empty(trim($_GET["productid"]))){
                 $row = $stmt->fetch(PDO::FETCH_ASSOC);
                 
                 // Retrieve individual field value
-                $thumbnaillink = $row["product_thumbnail_link"];
-                $productname = $row["product_name"];
-                $description = $row["product_description"];
-                $retailprice = $row["product_retail_price"];
+                $product_name = $row["product_name"];
+                $product_details = $row["product_details"];
+                $product_retail_price = $row["product_retail_price"];
             } else{
                 // URL doesn't contain valid id parameter. Redirect to error page
-                header("location: error.php");
+                header("location: ../public/error.php");
                 exit();
             }
             
@@ -45,7 +43,7 @@ if(isset($_GET["productid"]) && !empty(trim($_GET["productid"]))){
     unset($pdo);
 } else{
     // URL doesn't contain id parameter. Redirect to error page
-    header("location: error.php");
+    header("location: ../public/error.php");
     exit();
 }
 ?>
@@ -68,24 +66,20 @@ if(isset($_GET["productid"]) && !empty(trim($_GET["productid"]))){
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <h1 class="mt-5 mb-3">View Record</h1>
+                    <h1 class="mt-5 mb-3">View Product Record</h1>
                     <div class="form-group">
-                        <label>Thumbnail link</label>
-                        <p><b><?php echo $row["product_thumbnail_link"]; ?></b></p>
-                    </div>
-                    <div class="form-group">
-                        <label>Product name</label>
+                        <label>Product Name</label>
                         <p><b><?php echo $row["product_name"]; ?></b></p>
                     </div>
                     <div class="form-group">
-                        <label>Description</label>
-                        <p><b><?php echo $row["product_description"]; ?></b></p>
+                        <label>Product Details</label>
+                        <p><b><?php echo $row["product_details"]; ?></b></p>
                     </div>
                     <div class="form-group">
-                        <label>Retail price</label>
+                        <label>Retail Price</label>
                         <p><b><?php echo $row["product_retail_price"]; ?></b></p>
                     </div>
-                    <p><a href="index.php" class="btn btn-primary">Back</a></p>
+                    <p><a href="../index.php" class="btn btn-primary">Back</a></p>
                 </div>
             </div>        
         </div>
